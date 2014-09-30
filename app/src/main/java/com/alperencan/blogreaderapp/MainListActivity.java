1608011a6e19a1290eb4eff281828bbacdd5ed2f
Alp Eren Can <alperenc@gmail.com>
2014-09-30 12:47:10 +0300

    Create ListActivity and associated list view

--- app/src/main/java/com/alperencan/blogreaderapp/MainListActivity.java
+++ app/src/main/java/com/alperencan/blogreaderapp/MainListActivity.java
@@ -1,12 +1,12 @@
 package com.alperencan.blogreaderapp;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainListActivity extends Activity {
+public class MainListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
