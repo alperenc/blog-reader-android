92252e54cb6657c6cbe5f6b711819866cf43d07e
Alp Eren Can <alperenc@gmail.com>
2014-10-03 17:17:07 +0300

    Add functionality to view a webpage and an action bar button to share links

--- app/src/main/java/com/alperencan/blogreaderapp/MainListActivity.java
+++ app/src/main/java/com/alperencan/blogreaderapp/MainListActivity.java
@@ -3,15 +3,19 @@ package com.alperencan.blogreaderapp;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -25,35 +29,59 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mBlogPostTitles;
     public static final int NUMBER_OF_POSTS = 20;
     public static final String TAG = MainListActivity.class.getSimpleName();
     protected JSONObject mBlogData;
+    protected ProgressBar mProgressBar;
+
+    private final String KEY_TITLE = "title";
+    private final String KEY_AUTHOR = "author";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
         if (isNetworkAvailable()) {
+            mProgressBar.setVisibility(View.VISIBLE);
             GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
             getBlogPostsTask.execute();
         } else {
             Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
         }
 
-        // Resources resources = getResources();
-
-        // mBlogPostTitles = resources.getStringArray(R.array.android_names);
-
-        // ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
-        // setListAdapter(adapter);
-
         // Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        JSONArray jsonPosts = null;
+        try {
+            jsonPosts = mBlogData.getJSONArray("posts");
+            JSONObject jsonPost = jsonPosts.getJSONObject(position);
+            String blogUrl = jsonPost.getString("url");
+
+            Intent intent = new Intent(this, BlogWebViewActivity.class);
+            intent.setData(Uri.parse(blogUrl));
+            startActivity(intent);
+
+        } catch (JSONException e) {
+            logException(e);
+        }
+
+    }
+
+    private void logException(Exception e) {
+        Log.e(TAG, "Exception caught!: ", e);
     }
 
     private boolean isNetworkAvailable() {
@@ -69,51 +97,52 @@ public class MainListActivity extends ListActivity {
         return isAvailable;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void updateList() {
+    private void handleBlogResponse() {
+        mProgressBar.setVisibility(View.INVISIBLE);
         if (mBlogData == null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle(getString(R.string.title));
-            builder.setMessage(getString(R.string.error_message));
-            builder.setPositiveButton(android.R.string.ok, null);
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            updateDisplayForError();
         } else {
             try {
                 JSONArray jsonPosts = mBlogData.getJSONArray("posts");
-                mBlogPostTitles = new String[jsonPosts.length()];
+                ArrayList<HashMap<String, String>> blogPosts =
+                        new ArrayList<HashMap<String, String>>();
+
                 for (int i = 0; i < jsonPosts.length(); i++) {
                     JSONObject post = jsonPosts.getJSONObject(i);
-                    String title = post.getString("title");
+
+                    String title = post.getString(KEY_TITLE);
                     title = Html.fromHtml(title).toString();
-                    mBlogPostTitles[i] = title;
+                    String author = post.getString(KEY_AUTHOR);
+                    author = Html.fromHtml(author).toString();
+
+                    HashMap<String, String> blogPost = new HashMap<String, String>();
+                    blogPost.put(KEY_TITLE, title);
+                    blogPost.put(KEY_AUTHOR, author);
+
+                    blogPosts.add(blogPost);
                 }
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+                String[] keys = { KEY_TITLE, KEY_AUTHOR };
+                int[] ids = { android.R.id.text1, android.R.id.text2};
+                SimpleAdapter adapter = new SimpleAdapter(this, blogPosts, android.R.layout.simple_list_item_2, keys, ids);
+
                 setListAdapter(adapter);
             } catch (JSONException e) {
-                Log.e(TAG, "Exception caught!: ", e);
+                logException(e);
             }
         }
+    }
+
+    private void updateDisplayForError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.title));
+        builder.setMessage(getString(R.string.error_message));
+        builder.setPositiveButton(android.R.string.ok, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        TextView emptyTextView = (TextView) getListView().getEmptyView();
+        emptyTextView.setText(getString(R.string.no_items));
     }
 
     private class GetBlogPostsTask extends AsyncTask<Object, Void, JSONObject> {
@@ -147,11 +176,11 @@ public class MainListActivity extends ListActivity {
                 }
 
             } catch (MalformedURLException e) {
-                Log.e(TAG, "Exception caught!: ", e);
+                logException(e);
             } catch (IOException e) {
-                Log.e(TAG, "Exception caught!: ", e);
+                logException(e);
             } catch (Exception e) {
-                Log.e(TAG, "Exception caught!: ", e);
+                logException(e);
             }
             return jsonResponse;
         }
@@ -159,7 +188,7 @@ public class MainListActivity extends ListActivity {
         @Override
         protected void onPostExecute(JSONObject result) {
             mBlogData = result;
-            updateList();
+            handleBlogResponse();
         }
     }
 
